9f701d8f850e06bc82dd99539092c4af80fecbb8
Alex K <slantz@users.noreply.github.com>
2020-02-10 15:45:52 +0200

    added key (#8)

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -5,7 +5,7 @@ import { Icon } from "./Icon";
 
 export const Icons = ({icons, onPress, styleIconText}) => (
     <View>
-        {icons.map((icon) => <Icon icon={icon} onPress={onPress} styleIconText={styleIconText}/>)}
+        {icons.map((icon) => <Icon key={icon.index} icon={icon} onPress={onPress} styleIconText={styleIconText}/>)}
     </View>
 );
 
